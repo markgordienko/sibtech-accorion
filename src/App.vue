6ec3445ad25866218d9ee8529f62071618779a6d
markgordienko <mark.gordienko@gmail.com>
2023-12-03 19:40:31 +0700

    Добавил компонент AccordionItem
    Наполнил стартовую страницу элементами аккордеона

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="task-container">
+    <AccordionItem title="1ый элемент внешнего аккордеона">
+      <div>
+        Здесь находится наполнение 1го элемента внешнего аккордеона
+        <p>Какая-то информация в параграфе</p>
+        <AccordionItem title="1ый элемент внутреннего аккордеона">
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+            <AccordionItem
+              title="1ый элемент внутреннего внутреннего аккордеона"
+            >
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </div>
+            </AccordionItem>
+          </div>
+        </AccordionItem>
+        <AccordionItem
+          title="2ой элемент внутреннего аккордеона (развернут по-умолчанию)"
+          :initial-state="true"
+        >
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </div>
+        </AccordionItem>
+      </div>
+    </AccordionItem>
+    <AccordionItem title="2ой элемент внешнего аккордеона">
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </div>
+    </AccordionItem>
+    <AccordionItem title="3ий элемент внешнего аккордеона">
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </div>
+    </AccordionItem>
+    <AccordionItem title="4ый элемент внешнего аккордеона">
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </div>
+    </AccordionItem>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AccordionItem from "./components/AccordionItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AccordionItem,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.task-container {
+  width: 80%;
+  margin: auto;
+  padding-top: 10px;
 }
 </style>
---
+++ src/components/AccordionItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="accordion-container">
+    <div @click="toggleAccordion" class="accordion-header">
+      {{ title }}
+    </div>
+    <div class="accordion-content" :class="{ active: isOpen }">
+      <div class="content-inner">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isOpen: false,
+  }),
+  name: "AccordionItem",
+  props: {
+    title: String,
+    initialState: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {
+    this.isOpen = this.initialState;
+  },
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style>
+.accordion-container {
+  border: 1px solid black;
+  border-radius: 12px;
+  margin-top: 2px;
+}
+
+.accordion-header {
+  border: 1px solid #000000;
+  border-radius: 10px;
+  background-color: rgb(181, 132, 236);
+  color: white;
+  padding: 5px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.accordion-content {
+  overflow: hidden;
+  max-height: 0;
+  transition: max-height 0.8s linear;
+}
+
+.accordion-content.active {
+  max-height: 1000px;
+}
+
+.content-inner {
+  padding: 10px;
+}
+</style>
